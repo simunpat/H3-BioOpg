a432a3f930c1bacd5b199dbf7dea3387af2fdcbf
simunpat <spt@lunnar.fo>
2026-01-14 23:54:17 +0100

    changed nav to a sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 import { AuthService } from './core/auth/auth.service';
 
 @Component({
     selector: 'app-root',
-    imports: [CommonModule, RouterOutlet, RouterLink],
+    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, MatIconModule],
     templateUrl: './app.html',
     styleUrl: './app.scss',
 })
